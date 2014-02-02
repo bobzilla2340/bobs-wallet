f19a3f398120ce4d0fd62e5e7dd15a395cd323f6
Sai Naga Manoj Paladugu <paladugu.sainaga@gmail.com>
2014-02-02 12:32:42 -0500

    have to make new repo?? - sai

---
+++ src/cs2340/bobzilla/bobs_wallet/WelcomeActivity.java
@@ -0,0 +1,40 @@
+package cs2340.bobzilla.bobs_wallet;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class WelcomeActivity extends Activity {
+	Button mSigninButton;
+	Button mRegisterButton;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_welcome);
+		
+		mSigninButton = (Button)findViewById(R.id.signin);
+		mSigninButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(WelcomeActivity.this, "Sign In", Toast.LENGTH_SHORT)
+				.show();
+				
+			}
+		});
+		
+		mRegisterButton = (Button)findViewById(R.id.register);
+		mRegisterButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(WelcomeActivity.this, "Register", Toast.LENGTH_SHORT)
+				.show();
+				
+			}
+		});
+	}
+}

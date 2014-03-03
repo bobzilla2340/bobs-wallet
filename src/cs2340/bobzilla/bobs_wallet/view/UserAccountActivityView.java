489d8a5cf9fb9f9f5de7b35384d949ded0598586
Julia Ting <julia.ting@gatech.edu>
2014-03-02 20:24:02 -0500

    Refactored UserAccountActivity to follow the MVP model

--- src/cs2340/bobzilla/bobs_wallet/view/UserAccountActivityView.java
+++ src/cs2340/bobzilla/bobs_wallet/view/UserAccountActivityView.java
@@ -1,5 +1,13 @@
 package cs2340.bobzilla.bobs_wallet.view;
 
 public interface UserAccountActivityView {
+	
+	public String getWelcome();
+	
+	public String getAccountName();
+	
+	public String getInterestRate();
+	
+	public String getUserName();
 
 }

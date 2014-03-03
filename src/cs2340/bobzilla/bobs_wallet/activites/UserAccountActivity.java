4cf0ee64bea13ebf1ecf2247fb90f47aad5dcb7a
Julia Ting <julia.ting@gatech.edu>
2014-03-02 23:15:59 -0500

    Readding the refactoring to MVP of UserAccountActivity

--- src/cs2340/bobzilla/bobs_wallet/activites/UserAccountActivity.java
+++ src/cs2340/bobzilla/bobs_wallet/activites/UserAccountActivity.java
@@ -21,84 +21,60 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import cs2340.bobzilla.bobs_wallet.R;
-import cs2340.bobzilla.bobs_wallet.model.User;
-import cs2340.bobzilla.bobs_wallet.model.UserListSingleton;
+import cs2340.bobzilla.bobs_wallet.exceptions.InvalidAccountCreationException;
+import cs2340.bobzilla.bobs_wallet.view.UserAccountActivityView;
+import cs2340.bobzilla.bobs_wallet.presenter.UserAccountActivityPresenter;
 
-public class UserAccountActivity extends Activity {
 
-	// before we even get started, make sure this class
-	// implements the UserAccountAcitivityView interface!!!
-	// be sure to implement the appropriate methods... from
-	// the interface... I leave this for you to ponder.
+public class UserAccountActivity extends Activity implements UserAccountActivityView {
 	
-	// Allright, we need to make this into MVP
-	// First, get all the UI elements here.
-	// Get rid of user. you should not be directly
-	// interacting with a model element. Instead,
-	// modify the presenter code to get the list of
-	// already made FinanceAccount objects.
+	private TextView welcomeTextView;
+	private EditText accountNameEditText;
+	private EditText interestRateEditText;
+	private String userName;
+	private UserAccountActivityPresenter userAccountActivityPresenter;
 	
-	// MAKE THE USERACCOUNTACTIVITYPRESENTER AN INSTANCE FIELD!!!
-	
-	private User user;
 	private ListView listView;
 	private ArrayAdapter<String> arrayAdapter;
 	public static final String USER_FINANCE_ACCOUNT_NAME="cs2340.bobzilla.bobs_wallet.activities.UserAccountActivity.UserFinanceAccountName";
 	
-	
-	//in here, you can just set the UI elements to the proper
-	//reference. Also, make a private inner class for the
-	// OnItemClickListener - this makes the code SO MUCH CLEANER!!
-	// in here, you should also initialize the presenter.
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_user_account);
-		// Show the Up button in the action bar.
 		setupActionBar();
 		Log.e("CRITICAL", "UPDATE YOUR SD CARD");
-		
+
 		Intent userAccountIntent = getIntent();
-		String userName = userAccountIntent.getStringExtra(LoginActivity.LOGIN_USER_NAME);
-		user = UserListSingleton.getInstance().getUserList().getUser(userName);		
+		userName = userAccountIntent.getStringExtra(LoginActivity.LOGIN_USER_NAME);
 		
+		welcomeTextView = (TextView)findViewById(R.id.userAccountWelcomeMessage);
 		listView = (ListView)findViewById(R.id.userFinanceAccountList);
+		welcomeTextView.append(" " + userName + "!");
+		
 		arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.user_finance_account_list_text_view);
 		listView.setAdapter(arrayAdapter);
 		
-		setUpUserAccountScreen();
-		
-		listView.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> adapterView, View view, int arg0, long arg1) {
-				TextView clickedText = (TextView)view.findViewById(R.id.userFinanceAccountListTextView);
-				String clickedString = clickedText.getText().toString();
-				Toast.makeText(UserAccountActivity.this, "You clicked " + clickedString, Toast.LENGTH_SHORT).show();
-				Intent userFinanceAccountActivityIntent = new Intent(UserAccountActivity.this, UserFinanceAccountActivity.class);
-				userFinanceAccountActivityIntent.putExtra(USER_FINANCE_ACCOUNT_NAME, clickedString);
-				startActivity(userFinanceAccountActivityIntent);
-			}
-		});
-	}
-	
-	private void setUpUserAccountScreen() {
-		
-		TextView welcome = (TextView)findViewById(R.id.userAccountWelcomeMessage);
-		TextView userName = (TextView)findViewById(R.id.userAccountUserNamePrompt);
-		TextView firstName = (TextView)findViewById(R.id.userAccountFirstNamePrompt);
-		TextView lastName = (TextView)findViewById(R.id.userAccountLastNamePrompt);
-		TextView email = (TextView)findViewById(R.id.userAccountEMailPrompt);
-		
-		welcome.append(" " + user.getFirstName() + "!");
-		userName.append(" " + user.getUserName());
-		firstName.append(" " + user.getFirstName());
-		lastName.append(" " + user.getLastName());
-		email.append(" " + user.getEmail());
-		
-		
-		
-		for(String account: user.getFinanceAccountList().keySet()) {
+		userAccountActivityPresenter = new UserAccountActivityPresenter(this);
+
+		for(String account: userAccountActivityPresenter.getFinanceAccountNames(userName)) {
 			arrayAdapter.add(account);
+		}
+		
+		listView.setOnItemClickListener(new ListViewClickListener());
+	}
+
+	private class ListViewClickListener implements OnItemClickListener {
+
+		@Override
+		public void onItemClick(AdapterView<?> adapterView, View view, int arg0,
+				long arg1) {
+			TextView clickedText = (TextView)view.findViewById(R.id.userFinanceAccountListTextView);
+			String clickedString = clickedText.getText().toString();
+			Toast.makeText(UserAccountActivity.this, "You clicked " + clickedString, Toast.LENGTH_SHORT).show();
+			Intent userFinanceAccountActivityIntent = new Intent(UserAccountActivity.this, UserFinanceAccountActivity.class);
+			userFinanceAccountActivityIntent.putExtra(USER_FINANCE_ACCOUNT_NAME, clickedString);
+			startActivity(userFinanceAccountActivityIntent);			
 		}
 		
 	}
@@ -137,63 +113,48 @@ public class UserAccountActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 	
-	// pls completely refactor this method... you
-	// should make a private innner class that
-	// implements the DialogInterface.OnClickListener()
-	// interface.
 	public void onClick(View view) {
-		
 		LayoutInflater inflater = UserAccountActivity.this.getLayoutInflater();
 		final View alertDialogView = inflater.inflate(R.layout.alert_dialog_user_account_finance_account_create, null);
 		new AlertDialog.Builder(this)
 		.setTitle(R.string.user_account_finance_account_create_dialog_box)
 		.setNegativeButton("Quit", null)
-		.setPositiveButton("Make Account", new DialogInterface.OnClickListener() {
-			
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				
-				EditText accountName = (EditText)alertDialogView.findViewById(R.id.alertDialogUserFinanceAccountNameEditText);
-				EditText interestRate = (EditText)alertDialogView.findViewById(R.id.alertDialogUserFinanceAccountIntrestRateEditText);
-				Log.d("debugging message", "edit text: " + accountName);
-				String account = accountName.getText().toString();
-				double interest;
-				if(account.equals("")) {
-					Toast.makeText(UserAccountActivity.this, "Please enter a valid account name!", Toast.LENGTH_SHORT).show();
-				}
-				else if(interestRate.getText().toString().equals("")) {
-					Toast.makeText(UserAccountActivity.this, "Please enter a valid interest rate!", Toast.LENGTH_SHORT);
-				}
-				else if(Double.parseDouble(interestRate.getText().toString()) <= 0) {
-					Toast.makeText(UserAccountActivity.this, "Please enter a valid interest rate!", Toast.LENGTH_SHORT);
-				}
-				else {
-					interest = Double.parseDouble(interestRate.getText().toString());
-					user.addFinanceAccount(account, interest);
-					Toast.makeText(UserAccountActivity.this, "Account successfully created!", Toast.LENGTH_SHORT).show();
-					arrayAdapter.add(account);
-				}
-			}
-		})
+		.setPositiveButton("Make Account", new AlertDialogClickListener())
 		.setView(alertDialogView)
 		.show();
-		
-		
-		
-		
+		accountNameEditText = (EditText)alertDialogView.findViewById(R.id.alertDialogUserFinanceAccountNameEditText);
+		interestRateEditText = (EditText)alertDialogView.findViewById(R.id.alertDialogUserFinanceAccountIntrestRateEditText);
+
 	}
 	
-	// I would like to use this private inner class instead of the anonymous class.
-	// Make it so!
-	// Ideally, you should be making a call to the presenter here.
 	private class AlertDialogClickListener implements DialogInterface.OnClickListener {
-
+		
 		@Override
 		public void onClick(DialogInterface dialog, int which) {
-			// TODO Auto-generated method stub
-			
+			try {
+				userAccountActivityPresenter.onClick();
+				arrayAdapter.add(accountNameEditText.getText().toString());
+				Toast.makeText(UserAccountActivity.this, "Account successfully created!", Toast.LENGTH_SHORT).show();
+			} catch (InvalidAccountCreationException e) {
+				String toastMessage = e.getMessage();
+				Toast.makeText(UserAccountActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
+			}
 		}
-		
 	}
 	
+	@Override
+	public String getUserName() {
+		return userName;
+	}
+	
+	@Override
+	public String getAccountName() {
+		return accountNameEditText.getText().toString();
+	}
+	
+	@Override
+	public String getInterestRate() {
+		return interestRateEditText.getText().toString();
+	}
+
 }
--- src/cs2340/bobzilla/bobs_wallet/view/UserAccountActivityView.java
+++ src/cs2340/bobzilla/bobs_wallet/view/UserAccountActivityView.java
@@ -2,4 +2,11 @@ package cs2340.bobzilla.bobs_wallet.view;
 
 public interface UserAccountActivityView {
 
+	public String getAccountName();
+	
+	public String getUserName();
+	
+	public String getInterestRate();
+	
+	
 }

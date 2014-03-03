ed177625e71b9502da63b338f06af11e4fb2e5a2
Julia Ting <julia.ting@gatech.edu>
2014-03-02 23:42:38 -0500

    removing duplicate methods cause i dun goofed

--- src/cs2340/bobzilla/bobs_wallet/view/UserAccountActivityView.java
+++ src/cs2340/bobzilla/bobs_wallet/view/UserAccountActivityView.java
@@ -2,19 +2,10 @@ package cs2340.bobzilla.bobs_wallet.view;
 
 public interface UserAccountActivityView {
 	
-	public String getWelcome();
-	
 	public String getAccountName();
 	
 	public String getInterestRate();
 	
 	public String getUserName();
-
-	public String getAccountName();
-	
-	public String getUserName();
-	
-	public String getInterestRate();
-	
 	
 }

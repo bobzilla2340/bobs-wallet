0724a5d731b3e41db33106ffcd6daf63e8b1f00f
jmhwang7 <jmhwang7@gmail.com>
2014-03-13 00:23:48 -0400

    Added exception handling for specifying the dates for the reports.

--- src/cs2340/bobzilla/bobs_wallet/presenter/ReportActivityPresenter.java
+++ src/cs2340/bobzilla/bobs_wallet/presenter/ReportActivityPresenter.java
@@ -129,7 +129,7 @@ public class ReportActivityPresenter {
 	 * @param date2
 	 * @return
 	 */
-	private boolean isSameDay(Date date1, Date date2) {
+	public static boolean isSameDay(Date date1, Date date2) {
 		Calendar cal1 = Calendar.getInstance();
 		cal1.setTime(date1);
 		Calendar cal2 = Calendar.getInstance();

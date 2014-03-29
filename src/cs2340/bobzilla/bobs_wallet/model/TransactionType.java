828126bccda573b8dd54481074c4170c91c55228
fkhan0520 <fkhan0520@gmail.com>
2014-03-29 18:57:56 -0400

    Added Javadocs

--- src/cs2340/bobzilla/bobs_wallet/model/TransactionType.java
+++ src/cs2340/bobzilla/bobs_wallet/model/TransactionType.java
@@ -2,6 +2,11 @@ package cs2340.bobzilla.bobs_wallet.model;
 
 import java.io.Serializable;
 
+/**
+ * The TransactionType class is an enumerated type to designate
+ * transaction types.
+ * @author Farhan
+ */
 public enum TransactionType implements Serializable {
 	DEPOSIT, WITHDRAWAL
 }

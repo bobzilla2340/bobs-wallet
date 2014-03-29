6e618066dda846910892afabea0c880b8acabd83
fkhan0520 <fkhan0520@gmail.com>
2014-03-29 19:05:41 -0400

    Added Javadocs

--- src/cs2340/bobzilla/bobs_wallet/model/UserListSingleton.java
+++ src/cs2340/bobzilla/bobs_wallet/model/UserListSingleton.java
@@ -1,21 +1,37 @@
 package cs2340.bobzilla.bobs_wallet.model;
 
+/**
+ * The UserListSingleton class is used to access the UserList
+ * data structure.
+ * @author Farhan
+ */
 public class UserListSingleton {
 	
 	private static UserListSingleton userListSingleton;
 	private UserList userList;
-	
+
+    /**
+     * Creates a new UserListSingleton object.
+     */
 	public UserListSingleton() {
 		userList = new UserList();
 	}
-	
+
+    /**
+     * Gets the userListSingleton
+     * @return userListSingleton
+     */
 	public static UserListSingleton getInstance() {
 		if(userListSingleton == null) {
 			userListSingleton = new UserListSingleton();
 		}
 		return userListSingleton;
 	}
-	
+
+    /**
+     * Gets the user list.
+     * @return userList
+     */
 	public UserList getUserList() {
 		return userList;
 	}

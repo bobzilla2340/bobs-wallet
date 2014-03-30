f08a9c9ce3cc3bf8aaf17185be26d8c02b07dbb3
Sai Naga Manoj Paladugu <paladugu.sainaga@gmail.com>
2014-03-30 16:27:18 -0400

    I have made a package for test cases. -Sai

---
+++ src/cs2340/bobzilla/bobs_wallet/test/LoginPresenterTest.java
@@ -0,0 +1,14 @@
+package cs2340.bobzilla.bobs_wallet.test;
+
+import junit.framework.TestCase;
+
+/**
+ * This class will test if the login presenter class 
+ * works properly.
+ * @author sai
+ *
+ */
+public class LoginPresenterTest extends TestCase {
+
+	
+}

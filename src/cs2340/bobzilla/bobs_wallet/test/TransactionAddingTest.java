6b598a8e7d07adb1ea2044374ba512ab38532517
Jack <jcrawford42@gatech.edu>
2014-03-31 16:27:52 -0400

    making a different test

--- src/cs2340/bobzilla/bobs_wallet/test/TransactionAddingTest.java
+++ src/cs2340/bobzilla/bobs_wallet/test/TransactionAddingTest.java
@@ -14,41 +14,43 @@ import cs2340.bobzilla.bobs_wallet.model.TransactionType;
 public class TransactionAddingTest extends TestCase {
 
     public void testWithdrawal() {
-        FinanceAccount testAccount = new FinanceAccount("test",0);
-        testAccount.addTransaction(2340,TransactionType.WITHDRAWAL,"fun");
-        assertEquals(-2340.0,testAccount.getCurrentBalance());
-        assertEquals("test",testAccount.getAccountName());
+        FinanceAccount testAccount = new FinanceAccount("testing",0);
+        testAccount.addTransaction(1332,TransactionType.WITHDRAWAL,"fun");
+        assertEquals(-1332.0,testAccount.getCurrentBalance());
+        assertEquals("testing",testAccount.getAccountName());
         assertEquals(0.0,testAccount.getInterestRate());
         assertEquals(1, testAccount.getWithdrawals().size());
 
     }
 
     public void testDeposit() {
-        FinanceAccount testAccount = new FinanceAccount("test",0);
-        testAccount.addTransaction(2340,TransactionType.DEPOSIT,"fun");
-        assertEquals(2340.0, testAccount.getCurrentBalance());
-        assertEquals("test", testAccount.getAccountName());
+        FinanceAccount testAccount = new FinanceAccount("testing again",0);
+        testAccount.addTransaction(1301,TransactionType.DEPOSIT,"fun");
+        assertEquals(1301.0, testAccount.getCurrentBalance());
+        assertEquals("testing again", testAccount.getAccountName());
         assertEquals(0.0, testAccount.getInterestRate());
         assertEquals(1, testAccount.getDeposits().size());
 
     }
 
     public void testBoth() {
-        FinanceAccount testAccount = new FinanceAccount("test",0);
-        testAccount.addTransaction(2340,TransactionType.WITHDRAWAL,"fun");
-        assertEquals(-2340.0, testAccount.getCurrentBalance());
+        FinanceAccount testAccount = new FinanceAccount("testing",0);
+        testAccount.addTransaction(1332,TransactionType.WITHDRAWAL,"fun");
+        assertEquals(-1332.0,testAccount.getCurrentBalance());
+        assertEquals("testing",testAccount.getAccountName());
+        assertEquals(0.0,testAccount.getInterestRate());
         assertEquals(1, testAccount.getWithdrawals().size());
-        assertEquals(0, testAccount.getDeposits().size());
-        testAccount.addTransaction(2440, TransactionType.DEPOSIT, "fun");
-        assertEquals(100.0, testAccount.getCurrentBalance());
-        assertEquals(1, testAccount.getWithdrawals().size());
+        testAccount.addTransaction(1301,TransactionType.DEPOSIT,"fun");
+        assertEquals(-31.0, testAccount.getCurrentBalance());
+        assertEquals("testing again", testAccount.getAccountName());
+        assertEquals(0.0, testAccount.getInterestRate());
         assertEquals(1, testAccount.getDeposits().size());
         testAccount.addTransaction(100, TransactionType.WITHDRAWAL, "fun");
-        assertEquals(0.0, testAccount.getCurrentBalance());
+        assertEquals(-131.0, testAccount.getCurrentBalance());
         assertEquals(2, testAccount.getWithdrawals().size());
         assertEquals(1, testAccount.getDeposits().size());
         testAccount.addTransaction(5, TransactionType.DEPOSIT, "fun");
-        assertEquals(5.0, testAccount.getCurrentBalance());
+        assertEquals(-126.0, testAccount.getCurrentBalance());
         assertEquals(0.0, testAccount.getInterestRate());
         assertEquals(2, testAccount.getWithdrawals().size());
         assertEquals(2, testAccount.getDeposits().size());
